c102c110d504f3e78956638b4c2bcb91254acfdf
Christopher Trimboli <christopher.trimboli@protonmail.com>
2023-03-29 05:39:08 -0600

    add webpack for .mp4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,22 @@ const nextConfig = {
   },
   images: {
     domains: ['qa.tryspace.com', 'tryspace.com', 'localhost'],
-  }
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static/videos/",
+          }
+        }
+      ]
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
